35c06c29c1d8f60036c4cda0e2f5963986c7687a
deelylr <hananihakimi09@gmail.com>
2026-03-24 16:31:54 +0800

    Create Class3.cs

---
+++ MazeRunnerProject/Class3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeRunnerProject
+{
+	public class Player
+	{
+		public string Name { get; set; }
+		public int Health { get; set; }
+		public int Score { get; set; }
+
+		public void TakeDamage(int amount)
+		{
+			Health -= amount;
+			Console.WriteLine($"Carefull!! Damage taken by {Name} now is {amount}!! Your health now {Health}");
+
+		}
+		public void AddScore(int points)
+		{
+			Score += points;
+			Console.WriteLine($"{Name} gained {points} points! Total now is {Score}");
+		}
+	}
+}
